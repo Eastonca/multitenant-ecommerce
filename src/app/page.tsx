6475557f3a6552086da3920c140d9c3fcf406007
Eastonca <159653257+Eastonca@users.noreply.github.com>
2025-07-13 12:03:17 -0700

    modified global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
- return (
-    <div>
-        <p className="text-rose-500">
-            Hello world
-        </p>
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am a button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input" />
+        </div>
+        <div>
+          <Progress value={50}></Progress>
+        </div>
+        <div>
+          <Textarea placeholder="I am a text area" id=""></Textarea>
+        </div>
+        <div>
+            <Checkbox></Checkbox>
+        </div>
+      </div>
     </div>
- )
+  );
 }

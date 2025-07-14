16f863f56f08f337397a3e341e76b381363eba1c
Eastonca <159653257+Eastonca@users.noreply.github.com>
2025-07-14 16:07:05 -0700

    Home layout-fix

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -48,7 +48,7 @@ export default NavbarItem;
 
 export const Navbar = () => {
   const pathname = usePathname();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
